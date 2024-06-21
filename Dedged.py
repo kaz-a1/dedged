eaa5b5d2befcb8cd4c459e0aca4f1edc76b3484d
23110023 <kazusigesada54@gmail.com>
2024-06-22 03:37:32 +0900

    アルゴリズムの選択を追加および、シーク位置の決定

--- Dedged.py
+++ Dedged.py
@@ -7,12 +7,13 @@ import random
 import string
 import argparse
 
-def encrypt_file(input_file, times):
+#AESで暗号化
+def encrypt_file_AES(input_file, times):
 
     # ファイルの読み込み
     with open(input_file, 'rb') as f:
         data = f.read()
-    print(input_file + "is encrypted start.")
+    print(input_file + " is encrypted start.")
     for i in range(0, int(times)):
         # AESで使用する鍵とIV（Initialization Vector）を生成
         backend = default_backend()
@@ -25,10 +26,42 @@ def encrypt_file(input_file, times):
 
     # IVと暗号化されたデータをファイルに書き込む
     with open(input_file, 'wb') as f:
+        f.seek(0)
         f.write(iv + encrypted_data)
 
-    print(input_file + "is encrypted ok.")
+    print(input_file + " is encrypted ok.")
+#RSA暗号で暗号化
+def generate_rsa_key_pair():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+    public_key = private_key.public_key()
+    
+    return private_key, public_key
 
+def encrypt_file_RSA(input_file, public_key, times):
+    # ファイルの読み込み
+    with open(input_file, 'rb') as f:
+        data = f.read()
+    print(input_file + " is encrypted start.")
+    for i in (0,times):
+        # データをRSAで暗号化
+        encrypted_data = public_key.encrypt(
+            data,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+    # 暗号化されたデータをファイルに書き込む
+    with open(input_file, 'wb') as f:
+        f.seek(0)
+        f.write(encrypted_data)
+
+    print(input_file + " is encrypted ok.")
 def randomname(n):
     randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
     return ''.join(randlst)
@@ -51,19 +84,33 @@ if __name__ == "__main__":
     parser.add_argument("-remove", action="store_true", help="you could remove your encrypted file.")
     parser.add_argument("-t", help="you could be set ecryting times.", type=int, default=1)
     parser.add_argument("-times", help="you could be set ecryting times.", type=int, default=1)
+    parser.add_argument("-A", help="you could be set ecryting algorism.", type=str, default="AES")
+    parser.add_argument("-Algorism", help="you could be set ecryting algorism.", type=str, default="AES")
     args = parser.parse_args()
     if os.path.isdir(args.file):
         for input_file in get_files_in_directory(args.file):
             times = args.t if args.t != 1 else args.times if args.times != 1 else 1
-            # ファイルを暗号化
-            encrypt_file(input_file, times)
+            if args.A.upper() == "AES" or args.Algorism.upper() == "AES":
+                # ファイルを暗号化
+                encrypt_file_AES(input_file, times)
+            elif args.A.upper() == "RSA" or args.Algorism.upper() == "RSA":
+                # RSA鍵ペアの生成
+                private_key, public_key = generate_rsa_key_pair()
+                # ファイルの暗号化
+                encrypt_file_RSA(input_file, public_key, times)
 
         if args.rm or args.remove:
             shutil.rmtree(args.file)
     else:
         times = args.t if args.t != 1 else args.times if args.times != 1 else 1
-        encrypt_file(args.file, times)
+        if args.A.upper() == "AES" or args.Algorism.upper() == "AES":
+            # ファイルを暗号化
+            encrypt_file_AES(args.file, times)
+        elif args.A.upper() == "RSA" or args.Algorism.upper() == "RSA":
+            # RSA鍵ペアの生成
+            private_key, public_key = generate_rsa_key_pair()
+            # ファイルの暗号化
+            encrypt_file_RSA(args.file, public_key, times)
         if args.rm or args.remove:
             os.remove(args.file)
-            
     print("Encryption completed successfully.")
