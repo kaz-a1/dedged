a8bbe795a5ec4a0f7a7f739dabdd0f833dca48d4
haruki7049 <tontonkirikiri@gmail.com>
2024-06-30 12:40:19 +0900

    style: ruff-format

--- Dedged.py
+++ Dedged.py
@@ -10,40 +10,48 @@ import questionary
 import sys
 import logging
 
-#AESで暗号化
+
+# AESで暗号化
 def encrypt_file_AES(input_file, times):
     # ファイルの読み込み
     if os.path.exists(input_file) == False:
         print("file is not found.")
         sys.exit()
-    with open(input_file, 'rb') as f:
+    with open(input_file, "rb") as f:
         data = f.read()
     print(input_file + " is encrypted start.")
     for i in range(0, int(times)):
         # AESで使用する鍵とIV（Initialization Vector）を生成
         backend = default_backend()
         iv = os.urandom(16)  # IVはランダムに生成する
-        cipher = Cipher(algorithms.AES(randomname(32).encode('utf-8')[:32]), modes.CFB(iv), backend=backend)
+        cipher = Cipher(
+            algorithms.AES(randomname(32).encode("utf-8")[:32]),
+            modes.CFB(iv),
+            backend=backend,
+        )
         encryptor = cipher.encryptor()
 
         # データをAESで暗号化し、IVを先頭に追加
         encrypted_data = encryptor.update(data) + encryptor.finalize()
 
     # IVと暗号化されたデータをファイルに書き込む
-    with open(input_file, 'wb') as f:
+    with open(input_file, "wb") as f:
         f.seek(0)
         f.write(iv + encrypted_data)
 
     print(input_file + " is encrypted ok.")
-#RSA暗号で暗号化
+
+
+# RSA暗号で暗号化
 def generate_rsa_key_pair():
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
     )
     public_key = private_key.public_key()
-    
+
     return private_key, public_key
+
 
 def encrypt_file_RSA(input_file, public_key, times):
     # ファイルの読み込み
@@ -51,30 +59,32 @@ def encrypt_file_RSA(input_file, public_key, times):
         print("file is not found.")
         sys.exit()
     # ファイルの読み込み
-    with open(input_file, 'rb') as f:
+    with open(input_file, "rb") as f:
         data = f.read()
     print(input_file + " is encrypted start.")
-    for i in (0,times):
+    for i in (0, times):
         # データをRSAで暗号化
         encrypted_data = public_key.encrypt(
             data,
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()),
                 algorithm=hashes.SHA256(),
-                label=None
-            )
+                label=None,
+            ),
         )
 
     # 暗号化されたデータをファイルに書き込む
-    with open(input_file, 'wb') as f:
+    with open(input_file, "wb") as f:
         f.seek(0)
         f.write(encrypted_data)
 
     print(input_file + " is encrypted ok.")
 
+
 def randomname(n):
     randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
-    return ''.join(randlst)
+    return "".join(randlst)
+
 
 def get_files_in_directory(directory):
     file_list = []
@@ -86,20 +96,38 @@ def get_files_in_directory(directory):
             file_list.append(file_path)
     return file_list
 
+
 # メインの実行部分
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="please delete directory or file.")
-    parser.add_argument("-rm", action="store_true", help="you could remove your encrypted file.")
-    parser.add_argument("-remove", action="store_true", help="you could remove your encrypted file.")
-    parser.add_argument("-t", help="you could be set ecrypting times.", type=int, default=1)
-    parser.add_argument("-times", help="you could be set ecrypting times.", type=int, default=1)
-    parser.add_argument("-A", help="you could be set ecrypting algorism.", type=str, default="AES")
-    parser.add_argument("-Algorism", help="you could be set ecrypting algorism.", type=str, default="AES")
+    parser.add_argument(
+        "-rm", action="store_true", help="you could remove your encrypted file."
+    )
+    parser.add_argument(
+        "-remove", action="store_true", help="you could remove your encrypted file."
+    )
+    parser.add_argument(
+        "-t", help="you could be set ecrypting times.", type=int, default=1
+    )
+    parser.add_argument(
+        "-times", help="you could be set ecrypting times.", type=int, default=1
+    )
+    parser.add_argument(
+        "-A", help="you could be set ecrypting algorism.", type=str, default="AES"
+    )
+    parser.add_argument(
+        "-Algorism",
+        help="you could be set ecrypting algorism.",
+        type=str,
+        default="AES",
+    )
     parser.add_argument("-rf", action="store_true")
     args = parser.parse_args()
     while args.rf == False:
-        que = questionary.text('are you sure you want to delete or encrypt it? y(yes)/n(no)').ask()
+        que = questionary.text(
+            "are you sure you want to delete or encrypt it? y(yes)/n(no)"
+        ).ask()
         if que.upper() == "N" or que.upper() == "NO":
             sys.exit()
         elif que.upper() == "Y" or que.upper() == "YES":
